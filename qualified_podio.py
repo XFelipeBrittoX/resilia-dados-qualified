ead5d2d6507a6bcbe633f9618c9cf7cc141a0a75
Felipe Britto <felipe_br_ito@hotmail.com>
2021-11-25 14:32:53 -0300

    exercicio qualified

---
+++ qualified_podio.py
@@ -0,0 +1,13 @@
+def podio_olimpico(tempo_chegada1,tempo_chegada2,tempo_chegada3):
+    if tempo_chegada1 < tempo_chegada2 and tempo_chegada2 < tempo_chegada3:
+        return f'1 - {tempo_chegada1} minutos\n2 - {tempo_chegada2} minutos\n3 - {tempo_chegada3} minutos\n'
+    elif tempo_chegada1 < tempo_chegada3 and tempo_chegada3 < tempo_chegada2:
+        return f'1 - {tempo_chegada1} minutos\n2 - {tempo_chegada3} minutos\n3 - {tempo_chegada2} minutos\n'
+    elif tempo_chegada2 < tempo_chegada3 and tempo_chegada3 < tempo_chegada1:
+        return f'1 - {tempo_chegada2} minutos\n2 - {tempo_chegada3} minutos\n3 - {tempo_chegada1} minutos\n'
+    elif tempo_chegada2 < tempo_chegada1 and tempo_chegada1 < tempo_chegada3:
+        return f'1 - {tempo_chegada2} minutos\n2 - {tempo_chegada1} minutos\n3 - {tempo_chegada3} minutos\n'
+    elif tempo_chegada3 < tempo_chegada2 and tempo_chegada2 < tempo_chegada1:
+        return f'1 - {tempo_chegada3} minutos\n2 - {tempo_chegada2} minutos\n3 - {tempo_chegada1} minutos\n'
+    elif tempo_chegada3 < tempo_chegada1 and tempo_chegada1 < tempo_chegada2:
+        return f'1 - {tempo_chegada3} minutos\n2 - {tempo_chegada1} minutos\n3 - {tempo_chegada2} minutos\n'
